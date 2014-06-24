bef3215dddf18a61a48f9bc97873d96de08b12ae
CCPorg <coinz.stuff@gmail.com>
2014-06-24 21:00:46 +0200

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -22,11 +22,6 @@ namespace Checkpoints
             boost::assign::map_list_of
 //(0, uint256("0x"))
 (0,     uint256("0x5c5a2c6d59ef93212407c7fef081543bde06adfd24aed1d22da67ceb3444bd11"))
-(1,     uint256("0xbde1affe6853fa49f80c13c29c929777e831941ac993cae4ce6608817f4b3d8e"))
-(10,    uint256("0xe2e4eb0938c10f36e82d8480459efdbb4e404fc8bbf8876ed837119e43a8d5e8"))
-(20,    uint256("0xf1aa89b2278d5c772c663bd814dac9b96e3da3b7c563f2f97ca101804afb40a9"))
-(30,    uint256("0x3adcb672517b1c8f75430289d5b56d86cf410b11e959b4ce7518c4f22e7dd991"))
-(33,    uint256("0xfd2c0ecb617214cc533718a2f52897ca597ab962fe435eb5570c68d1eba41668")) //"time" : 1403564846,
 ;
 
 
